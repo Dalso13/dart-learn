fa81ace9c42a9929f1c15d1e8394bcde472399a7
Dalso <flskwhdhk@gmail.com>
2023-11-28 18:17:17 +0900

    369 문제 최종 수정

--- lib/challenge/Challenge369.dart
+++ lib/challenge/Challenge369.dart
@@ -4,75 +4,52 @@ main() {
   var player3 = Player("player3");
   var player4 = Player("player4");
 
-  int currentPlayerindex = 1;
+  List<Player> players = [Player("player1"),Player("player2"),Player("player3"),Player("player4")];
+
+  int currentPlayerindex = 0;
 
   int clap = 0;
   int rool = 0;
   int ahh = 0;
 
   for (int i = 1; i<=100; i++){
-    late var player;
-    switch(currentPlayerindex){
-      case 1 : player = player1;
-        break;
-      case 2 : player = player2;
-        break;
-      case 3 : player = player3;
-        break;
-      case 4 : player = player4;
-        break;
-    }
    
     if (i % 30 == 0){
-      print("${player.name} : ahh");
+      print("${players[currentPlayerindex].name} : ahh");
       ahh++;
     } else if(i % 10 == 0) {
-      print("${player.name} : rool");
+      print("${players[currentPlayerindex].name} : rool");
       rool++;
     } else if(i % 3 == 0) {
-      print("${player.name} : clap");
-      player.clapUp();
+      print("${players[currentPlayerindex].name} : clap");
+      players[currentPlayerindex].clapUp();
       clap++;
     } else {
-      print("${player.name} $i");
-    }
-
-
-    if (currentPlayerindex == 4){
-      currentPlayerindex = 0;
+      print("${players[currentPlayerindex].name} $i");
     }
 
     currentPlayerindex++;
+
+    if (currentPlayerindex == players.length){
+      currentPlayerindex = 0;
+    }
+
   }
 
   print("clap의 총 갯수 $clap");
   print("rool 총 갯수 $rool");
   print("ahh의 총 갯수 $ahh");
-  print("player1 ${player1.clap}");
-  print("player2 ${player2.clap}");
-  print("player3 ${player3.clap}");
-  print("player4 ${player4.clap}");
-
-  var bestPlayer = player1;
-
-  for(int i = 2; i < 5; i++){
-    late var chlPlayer;
-    switch(i){
-      case 2 : chlPlayer = player2;
-        break;
-      case 3 : chlPlayer = player3;
-          break;
-      case 4 : chlPlayer = player4;
-          break;
-    }
-
-    bestPlayer.clap < chlPlayer.clap ? bestPlayer = chlPlayer : false;
+  int bestPlayer = 0;
+  for (int i = 0; i< players.length; i++){
+    print("${players[i].name} ${players[i].clap}");
+  }
+  for(int i = 0; i < players.length -1; i++){
+    bestPlayer = players[i].clap < players[i+1].clap ?  i + 1 : bestPlayer;
   }
 
-  print("가장 많은 clap을 출력한 플레이어 : ${bestPlayer.name}" );
+  print("가장 많은 clap을 출력한 플레이어 : ${players[bestPlayer].name}" );
 }
 
-// 팀원들과 공유를 위해 직관적으로 작성하느라 메모리 낭비 및 코드가 더러워진점이 많습니다.
 
 class Player {
   int clap = 0;
