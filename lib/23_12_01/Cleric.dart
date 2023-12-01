4f1fade2341c6e0741e56f2fe6f1a53e94db4dd8
Dalso <flskwhdhk@gmail.com>
2023-12-01 14:18:58 +0900

    연습 문제 2 - 1

---
+++ lib/23_12_01/Cleric.dart
@@ -0,0 +1,32 @@
+import 'dart:math';
+
+class Cleric {
+  String? name;
+  int _hp = 50;
+  int _mp = 10;
+  static final int maxHp = 50;
+  static final int maxMp = 10;
+
+  void selfAid() {
+    if (_mp < 5) {
+      return;
+    }
+    _mp -= 5;
+    _hp = maxHp;
+  }
+
+  int pray(int a) {
+    int ran = Random().nextInt(3);
+    int recovery = ran + a;
+
+    if (recovery + _mp > maxMp) {
+      recovery = maxMp - _mp;
+      _mp = maxMp;
+    } else {
+      _mp += recovery;
+    }
+
+    return recovery;
+  }
+}
+
