487f21dce83e6532805cfe10a9e92d9d68851932
Dalso <flskwhdhk@gmail.com>
2023-12-01 14:44:57 +0900

    문법 수정

--- lib/23_11_30/exercise/main.dart
+++ lib/23_11_30/exercise/main.dart
@@ -1,5 +1,4 @@
 import 'Cleric.dart';
-
 main(){
   Cleric cleric = Cleric();
 }
--- lib/23_12_01/Test.dart
+++ lib/23_12_01/Test.dart
@@ -4,6 +4,6 @@ import 'package:untitled/23_12_01/Cleric.dart';
 
 main(){
   Cleric c1 =  Cleric("아서스", hp: 35);
-  Cleric c2 =  Cleric("아서스", hp: 35, mp: 10);
+  Cleric c2 =  Cleric("아서스", hp: 35, mp: 8);
   Cleric c3 =  Cleric("아서스");
 }
