87f909e8abd617fb8b0d89af157bbf8b0b705423
Dalso <flskwhdhk@gmail.com>
2023-12-04 10:47:59 +0900

    getter setter 구현

--- lib/23_12_04/getterAndSetter.dart
+++ lib/23_12_04/getterAndSetter.dart
@@ -1,9 +1,16 @@
 class Test{
   int _hp = 50;
 
-
-  int getHp() => _hp;
+  //다른 언어에서 쓰는법
+  /*int getHp() => _hp;
   void setHp(int a) => {
+    _hp = _hp + a
+  };*/
+  
+  
+  // dart 사용법
+  int get hp => _hp;
+  set hp(int a) => {
     _hp = _hp + a
   };
 
