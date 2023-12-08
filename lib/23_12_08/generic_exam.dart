257f7a869d493731dccafc86e197875b33843b50
Dalso <flskwhdhk@gmail.com>
2023-12-08 15:52:03 +0900

    제네릭, 열거형 연습문제 2

--- lib/23_12_08/generic_exam.dart
+++ lib/23_12_08/generic_exam.dart
@@ -1,11 +1,48 @@
-class StringBox<E> {
-  E? _put;
+class StringBox<E extends KeyType> {
+  E _put;
+  int padlock = 0;
+  int button  = 0;
+  int dial  = 0;
+  int finger  = 0;
 
+  StringBox(this._put);
 
-  set put(E put){
+  set setPut(E put){
     _put = put;
   }
 
-  get get => _put;
+  KeyType? get() {
+    switch(_put) {
+      case KeyType.padlock:
+        padlock++;
+        return padlock >= 1024 ? KeyType.padlock : null;
+      case KeyType.button:
+        button++;
+        return button >= 10000 ? KeyType.button : null;
+      case KeyType.dial:
+        dial++;
+        return dial >= 30000 ? KeyType.dial : null;
+      case KeyType.finger:
+        finger++;
+        return finger >= 1000000 ? KeyType.finger : null;
+    }
+  }
+
 }
 
+enum KeyType {
+  padlock,
+  button,
+  dial,
+  finger
+}
+
+main() {
+  StringBox<KeyType> key = StringBox(KeyType.button);
+
+  for(int i = 0; i < 9999; i++){
+    key.get();
+  }
+
+  print(key.get());
+}
