c2fd09a15536ebababfc8112c532964d389c307a
Dalso <flskwhdhk@gmail.com>
2023-12-08 15:52:25 +0900

    Dart 문자열 조작 연습문제

---
+++ lib/23_12_08/String_exam.dart
@@ -0,0 +1,52 @@
+
+main() {
+
+  Word word = Word(word: "hello");
+
+  print(word.isVowel(2));
+  print(word.isConsonant(2));
+}
+
+
+class Word {
+  String word = '';
+  final List<String> checked = ["a",'e','i','o','u'];
+  Word({
+    required this.word,
+  });
+
+  bool isVowel(int i) {
+    bool isVowel = false;
+
+    if(i > word.length || i < 1){
+      print("잘못된 인자");
+    } else {
+      for (var check in checked) {
+          if(word.substring(i-1,i) == check){
+            isVowel = true;
+            break;
+          }
+      }
+    }
+    return isVowel;
+  }
+
+  bool isConsonant(int i) {
+    bool isVowel = true;
+
+    if(i > word.length || i < 1) {
+      isVowel = false;
+      print("잘못된 인자");
+    } else {
+      for (var check in checked) {
+        if(word.substring(i-1,i) == check){
+          isVowel = false;
+          break;
+        }
+      }
+    }
+    return isVowel;
+  }
+
+
+}
