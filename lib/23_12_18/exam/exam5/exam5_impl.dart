c062c731baf10ca8fd381624505573dca0eebae4
Dalso <flskwhdhk@gmail.com>
2023-12-18 15:31:45 +0900

    5번 문제 껍데기만 만듬

---
+++ lib/23_12_18/exam/exam5/exam5_impl.dart
@@ -0,0 +1,30 @@
+import 'package:untitled/23_12_18/exam/exam5/exam5.dart';
+import 'package:untitled/23_12_18/exam/exam5/post.dart';
+
+class PostFileRepositoryImpl implements PostRepository {
+
+  @override
+  Future<void> addPost(Post post) {
+    // TODO: implement addPost
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> deletePost(Post post) {
+    // TODO: implement deletePost
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Post>> getPosts() {
+    // TODO: implement getPosts
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updatePost(Post post) {
+    // TODO: implement updatePost
+    throw UnimplementedError();
+  }
+  
+}
